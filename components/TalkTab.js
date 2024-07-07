065e78fe02bd0b2439fb7e01f5397658d3057782
mubarakone <52806204+mubarakone@users.noreply.github.com>
2024-07-06 23:51:00 -0700

    Update TalkTab.js

--- components/TalkTab.js
+++ components/TalkTab.js
@@ -8,16 +8,18 @@ export default function TalkTab({ switchToEditTab }) {
   const { db } = useFirebase();
   const [draftData, setDraftData] = useState([]);
   const [selectedDraft, setSelectedDraft] = useState(null);
+  const [rowCount, setRowCount] = useState(0);
 
   useEffect(() => {
     const fetchDraftData = async () => {
       try {
         const querySnapshot = await getDocs(collection(db, 'drafts'));
         const drafts = querySnapshot.docs.map(doc => ({
-          id: doc.id,
-          ...doc.data(),
-        }));
+            id: doc.id,
+            ...doc.data(),
+        })).filter(draft => draft.title && draft.description);  // Filter out drafts without title or description  
         setDraftData(drafts);
+        setRowCount(drafts.length); // Update the row count
       } catch (error) {
         console.error("Error fetching draft data: ", error);
       }
@@ -30,26 +32,26 @@ export default function TalkTab({ switchToEditTab }) {
 
   return (
     <div>
-        {/* <!-- Login Alert --> */}
-        <div class="bg-yellow-50 border border-yellow-200 text-sm text-yellow-800 rounded-lg p-4 dark:bg-yellow-800/10 dark:border-yellow-900 dark:text-yellow-500" role="alert">
-          <div class="flex">
-            <div class="flex-shrink-0">
-              <svg class="flex-shrink-0 size-4 mt-0.5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                <path d="m21.73 18-8-14a2 2 0 0 0-3.48 0l-8 14A2 2 0 0 0 4 21h16a2 2 0 0 0 1.73-3Z"></path>
-                <path d="M12 9v4"></path>
-                <path d="M12 17h.01"></path>
-              </svg>
-            </div>
-            <div class="ms-4">
-              <h3 class="text-sm text-red-700 font-semibold">
-                You are not logged in.
-              </h3>
-              <div class="mt-1 text-sm text-black">
-                Your IP address will be publicly visible if you make any edits. If you <Link className='text-sm text-semibold' href={"#"}>log in</Link> or <Link className='text-sm text-semibold' href={"#"}>create an account</Link>, your edits will be attributed to a username, among other <Link className='text-sm' href={"#"} >benefits</Link>.
-              </div>
+      {/* <!-- Login Alert --> */}
+      <div className="bg-yellow-50 border border-yellow-200 text-sm text-yellow-800 rounded-lg p-4 dark:bg-yellow-800/10 dark:border-yellow-900 dark:text-yellow-500" role="alert">
+        <div className="flex">
+          <div className="flex-shrink-0">
+            <svg className="flex-shrink-0 size-4 mt-0.5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+              <path d="m21.73 18-8-14a2 2 0 0 0-3.48 0l-8 14A2 2 0 0 0 4 21h16a2 2 0 0 0 1.73-3Z"></path>
+              <path d="M12 9v4"></path>
+              <path d="M12 17h.01"></path>
+            </svg>
+          </div>
+          <div className="ms-4">
+            <h3 className="text-sm text-red-700 font-semibold">
+              You are not logged in.
+            </h3>
+            <div className="mt-1 text-sm text-black">
+              Your IP address will be publicly visible if you make any edits. If you <Link className='text-sm text-semibold' href={"#"}>log in</Link> or <Link className='text-sm text-semibold' href={"#"}>create an account</Link>, your edits will be attributed to a username, among other <Link className='text-sm' href={"#"}>benefits</Link>.
             </div>
           </div>
         </div>
+      </div>
       {/* <!-- Table Section --> */}
       <div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
         {/* <!-- Card --> */}
@@ -61,7 +63,6 @@ export default function TalkTab({ switchToEditTab }) {
                 <div className="px-6 py-4 grid gap-3 md:flex md:justify-between md:items-center border-b border-gray-200 dark:border-neutral-700">
                   {selectedDraft ? (
                     <>
-                      <button onClick={() => setSelectedDraft(null)} className="text-blue-600 dark:text-blue-500 hover:underline">Back</button>
                       <h2 className="text-xl font-semibold text-gray-800 dark:text-neutral-200">{selectedDraft}</h2>
                     </>
                   ) : (
@@ -75,7 +76,7 @@ export default function TalkTab({ switchToEditTab }) {
                           {/* Filter Button */}
                           <div className="hs-dropdown [--placement:bottom-right] relative inline-block" data-hs-dropdown-auto-close="inside">
                             <button id="hs-as-table-table-filter-dropdown" type="button" className="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-800">
-                              <svg className="flex-shrink-0 size-3.5 text-gray-800 dark:text-neutral-200" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                              <svg className="flex-shrink-0 size-3.5 text-gray-800 dark:text-neutral-200" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                                 <path d="M3 6h18" />
                                 <path d="M7 12h10" />
                                 <path d="M10 18h4" />
@@ -139,7 +140,7 @@ export default function TalkTab({ switchToEditTab }) {
                         <th scope="col" className="px-6 py-3 text-start">
                           <a className="group inline-flex items-center gap-x-2" href="#">
                             <span className="text-xs font-semibold uppercase tracking-wide text-gray-800 dark:text-neutral-200">Title</span>
-                            <svg className="flex-shrink-0 size-3.5 text-gray-800 dark:text-neutral-200" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                            <svg className="flex-shrink-0 size-3.5 text-gray-800 dark:text-neutral-200" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                               <path d="m7 15 5 5 5-5" />
                               <path d="m7 9 5-5 5 5" />
                             </svg>
@@ -148,7 +149,7 @@ export default function TalkTab({ switchToEditTab }) {
                         <th scope="col" className="px-6 py-3 text-start">
                           <a className="group inline-flex items-center gap-x-2" href="#">
                             <span className="text-xs font-semibold uppercase tracking-wide text-gray-800 dark:text-neutral-200">Description</span>
-                            <svg className="flex-shrink-0 size-3.5 text-gray-800 dark:text-neutral-200" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                            <svg className="flex-shrink-0 size-3.5 text-gray-800 dark:text-neutral-200" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                               <path d="m7 15 5 5 5-5" />
                               <path d="m7 9 5-5 5 5" />
                             </svg>
@@ -157,7 +158,7 @@ export default function TalkTab({ switchToEditTab }) {
                         <th scope="col" className="px-6 py-3 text-start">
                           <a className="group inline-flex items-center gap-x-2" href="#">
                             <span className="text-xs font-semibold uppercase tracking-wide text-gray-800 dark:text-neutral-200">Level</span>
-                            <svg className="flex-shrink-0 size-3.5 text-gray-800 dark:text-neutral-200" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                            <svg className="flex-shrink-0 size-3.5 text-gray-800 dark:text-neutral-200" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                               <path d="m7 15 5 5 5-5" />
                               <path d="m7 9 5-5 5 5" />
                             </svg>
@@ -173,6 +174,44 @@ export default function TalkTab({ switchToEditTab }) {
                     </tbody>
                   </table>
                 )}
+                {/* Footer Section */}
+                {selectedDraft ? (
+                    <>
+                <div class="px-6 py-4 grid gap-3 md:flex md:justify-between md:items-center border-t border-gray-200 dark:border-neutral-700">
+                  <div>
+                    <div class="inline-flex gap-x-2">
+                      <button onClick={() => setSelectedDraft(null)} type="button" class="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-800">
+                        <svg class="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m15 18-6-6 6-6"/></svg>
+                        Back
+                      </button>
+                    </div>
+                  </div>
+                </div>
+                    </>
+                  ) : (
+                    <>
+                <div class="px-6 py-4 grid gap-3 md:flex md:justify-between md:items-center border-t border-gray-200 dark:border-neutral-700">
+                  <div>
+                    <p class="text-sm text-gray-600 dark:text-neutral-400">
+                      <span class="font-semibold text-gray-800 dark:text-neutral-200">{rowCount}</span> results
+                    </p>
+                  </div>
+                  <div>
+                    <div class="inline-flex gap-x-2">
+                      <button type="button" class="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-800">
+                        <svg class="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m15 18-6-6 6-6"/></svg>
+                        Prev
+                      </button>
+                      <button type="button" class="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-800">
+                        Next
+                        <svg class="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m9 18 6-6-6-6"/></svg>
+                      </button>
+                    </div>
+                  </div>
+                </div>
+                    </>
+                    )}
+                {/* End Footer Section */}
               </div>
             </div>
           </div>
@@ -212,14 +251,14 @@ function DraftRow({ draftTitle, description, level, onClick }) {
         <div className="px-6 py-2">
           <div className="hs-dropdown [--placement:bottom-right] relative inline-block">
             <button id="hs-table-dropdown-1" type="button" className="hs-dropdown-toggle py-1.5 px-2 inline-flex justify-center items-center gap-2 rounded-lg text-gray-700 align-middle disabled:opacity-50 disabled:pointer-events-none focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 transition-all text-sm dark:text-neutral-400 dark:hover:text-white dark:focus:ring-offset-gray-800">
-              <svg className="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="1"/><circle cx="19" cy="12" r="1"/><circle cx="5" cy="12" r="1"/></svg>
+              <svg className="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="1"/><circle cx="19" cy="12" r="1"/><circle cx="5" cy="12" r="1"/></svg>
             </button>
             <div className="hs-dropdown-menu transition-[opacity,margin] duration hs-dropdown-open:opacity-100 opacity-0 hidden divide-y divide-gray-200 min-w-40 z-20 bg-white shadow-2xl rounded-lg p-2 mt-2 dark:divide-neutral-700 dark:bg-neutral-800 dark:border dark:border-neutral-700" aria-labelledby="hs-table-dropdown-1">
               <div className="py-2 first:pt-0 last:pb-0">
                 <span className="block py-2 px-3 text-xs font-medium uppercase text-gray-400 dark:text-neutral-600">Actions</span>
-                <a className="flex items-center gap-x-3 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:hover:text-neutral-300" href="#">Rename draft</a>
+                <a className="flex items-center gap-x-3 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:hover:text-neutral-300" href="#">Rename team</a>
                 <a className="flex items-center gap-x-3 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:hover:text-neutral-300" href="#">Add to favorites</a>
-                <a className="flex items-center gap-x-3 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:hover:text-neutral-300" href="#">Archive draft</a>
+                <a className="flex items-center gap-x-3 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:hover:text-neutral-300" href="#">Archive team</a>
               </div>
               <div className="py-2 first:pt-0 last:pb-0">
                 <a className="flex items-center gap-x-3 py-2 px-3 rounded-lg text-sm text-red-600 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-red-500 dark:hover:bg-neutral-700 dark:hover:text-neutral-300" href="#">Delete</a>
