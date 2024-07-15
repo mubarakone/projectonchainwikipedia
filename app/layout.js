6604a65aec52cdda02696052083c7d5041f8c05b
mubarakone <52806204+mubarakone@users.noreply.github.com>
2024-07-15 15:26:02 -0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -3,13 +3,19 @@ import { FirebaseProvider } from '../context/FirebaseContext'
 
 export const metadata = {
   title: 'Project Onchain Wikipedia - Wikipedia',
-  icon: '/favicon.png',
+  icons: {
+    icon: '/favicon.png',
+    apple: '/favicon.png',
+  },
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <link rel="icon" href="/favicon.ico" />
+      <head>
+        <link rel="icon" href="/favicon.png" sizes="any" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon.png" />
+      </head>
       <body className="flex flex-col min-h-screen">
         <FirebaseProvider>
           {children}
